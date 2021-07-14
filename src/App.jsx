6802a371922dfb92740263437afab643fd299fc3
안광수 <jhk7716@gmail.com>
2021-07-14 17:37:26 +0900

    반응형 헤더 진행중

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 import Home from './pages/Home'
-import Introduce from './pages/Introduce'
+import Login from './pages/Login'
 import WebGame from './pages/WebGame'
 import Portfolio from './pages/Portfolio'
 
@@ -11,7 +11,7 @@ const App = () => {
       <div>
         <Switch>
           <Route exact path="/" component={Home}/>
-          <Route path="/introduce" component={Introduce}/>
+          <Route path="/login" component={Login}/>
           <Route path="/webGame" component={WebGame}/>
           <Route path="/portfolio" component={Portfolio}/>
         </Switch>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,16 @@ import '../assets/style/Header.css'
 
 const Header = () => {
     return (
-        <div className="header_wrap">
-            <Link to="/">Home</Link>
-            <Link to="/introduce">Introduce</Link>
-            <Link to="/webGame">WebGame</Link>
-            <Link to="/portfolio">Portfoli</Link>
+        <div>
+            <div className="header_web">
+                <Link to="/">Home</Link>
+                <Link to="/webGame">WebGame</Link>
+                <Link to="/portfolio">Portfoli</Link>
+                <Link to="/login">Login</Link>
+            </div>
+            <div className="header_mobile">
+                <i class="mdi mdi-view-headline"></i>
+            </div>
         </div>
     )
 }
